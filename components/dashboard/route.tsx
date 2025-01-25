ecb3f5facf86c92f1c0e9784de1b07c93ba63f41
dylanwong6605 <dylanwong6605@gmail.com>
2025-01-25 15:39:10 -0800

    fixed geist font typo

--- components/dashboard/route.tsx
+++ components/dashboard/route.tsx
@@ -1,10 +1,11 @@
 import { View, Text} from "react-native";
 import { alt_route } from "assets\icons\alt_route.png"
+import { Image } from "react-native-reanimated/lib/typescript/Animated";
 
 const Route = ({ routeName, eta }) => {
-    <View className = "w-5/6 flex flex-row text-white ">
+    <View className = "w-5/6 flex flex-row font-[Geist] text-white ">
         <View>
-            
+            <Image source = {alt_route} />
         </View>
         <View className = "text-xl">
             <Text className = "">
