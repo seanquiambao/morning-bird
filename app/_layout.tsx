b347bfab7bc3e57f29a297a215d0755a24fc3119
seanquiambao <slquiambao03@gmail.com>
2025-01-25 14:34:27 -0800

    PLEASE FINALLY

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import "../globals.css";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
