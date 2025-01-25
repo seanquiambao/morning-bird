345f6116c962d371a406ed91add768765dcd6773
seanquiambao <slquiambao03@gmail.com>
2025-01-25 15:35:47 -0800

    frontend

---
+++ components/routes/edit.tsx
@@ -0,0 +1,34 @@
+import {
+  View,
+  Text,
+  TextInput,
+  ImageBackground,
+  Image,
+  Pressable,
+} from "react-native";
+import bg1 from "@/assets/images/bg1.png";
+import directions from "@/assets/icons/directions.png";
+const DestinationScreen = () => {
+  return (
+    <ImageBackground source={bg1} className="flex flex-col p-4 h-full">
+      <View className="flex flex-col gap-4">
+        <Image source={directions} className="self-center" />
+        <TextInput
+          value="UCR"
+          className="text-white self-center font-bold text-6xl"
+        />
+      </View>
+      <View className="flex flex-col gap-4">
+        <Text className="font-bold text-white">Location</Text>
+        <TextInput className="bg-white/25 rounded-md p-4" />
+        <Pressable className="border rounded-md border-white">
+          <Text className="text-white font-bold p-5 self-center text-2xl">
+            Submit
+          </Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default DestinationScreen;
--- components/routes/index.tsx
+++ components/routes/index.tsx
@@ -1,8 +1,11 @@
 import { View, Text } from "react-native";
+import { Href, Link } from "expo-router";
 const RoutesScreen = () => {
   return (
     <View>
-      <Text>Hello</Text>
+      <Link href={"/routes/edit" as Href}>
+        <Text>Hello</Text>
+      </Link>
     </View>
   );
 };
