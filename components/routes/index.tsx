8fd3d82959974767ecf04a301531162b328f966e
seanquiambao <slquiambao03@gmail.com>
2025-01-26 02:18:32 -0800

    lol jk

--- components/routes/index.tsx
+++ components/routes/index.tsx
@@ -37,7 +37,7 @@ const RoutesScreen = () => {
     fetchRoutes().then((data) => {
       setItems(data.routes);
     });
-  }, []);
+  }, [location.source, location.destination]);
   return (
     <View>
       <ImageBackground
