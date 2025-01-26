5bbacc8fa0648b0b6bb1105c510cb873fe63409f
Zami Alam <zalam007@ucr.edu>
2025-01-25 22:32:39 -0800

    fixed axels location destruction
    axel is bully big boss that messed everything up and blamed it on me and forced me to fix it

--- components/dashboard/Location.tsx
+++ components/dashboard/Location.tsx
@@ -3,25 +3,24 @@ import { Href, Link } from "expo-router";
 import Route from "@/components/routes/route";
 
 interface props {
-  locationsID?: string;
+  locationsID: string;
+  locationName: string;
   routeName: string;
   eta: string;
 }
 
-const Location = ({ routeName, eta }: props) => {
+const Location = ({ locationsID, locationName, routeName, eta }: props) => {
   return (
     <View className="bg-white/10 rounded-3xl w-5/6 flex flex-col p-5">
-      <Link href={`/routes/0` as Href}>
-        <View className="flex items-start">
-          <Text className="text-5xl text-white font-bold font-[Geist] p-2">
-            {routeName}
-          </Text>
-          <Text className="text-xl text-white font-bold font-[Geist]">
-            Optimal Route
-          </Text>
-          <View className="">
-            <Route routeName={routeName} eta={eta} />
-          </View>
+        <Link href={`/routes/${locationsID}` as Href}>
+        <View className="">
+            <View className="flex items-start">
+                <Text className="text-5xl text-white font-bold font-[Geist]"> {locationName} </Text>
+                <Text className="text-xl text-white font-bold font-[Geist]"> Optimal Route </Text>   
+            </View>
+            <View className="">
+                <Route routeName={routeName} eta={eta} />
+            </View> 
         </View>
       </Link>
     </View>
--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -1,41 +1,25 @@
 import { Href, Link } from "expo-router";
-import { Text, View } from "react-native";
-import { useLocation } from "../context/location-context";
-import { useEffect } from "react";
+import { ImageBackground, Text, View } from "react-native";
+import React from "react";
+import dashboardgradient from "@/assets/images/dashboard-gradient.png";
+import AddLocation from "./AddLocation";
+import Location from "./Location";
+
 const DashboardScreen = () => {
-  const { location, setLocation } = useLocation();
-
-  useEffect(() => {
-    if (!location.source || !location.destination) {
-      console.log("Nope!");
-      return;
-    }
-
-    const fetchRoutes = async () => {
-      try {
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/directions/json?origin=${location.source}&destination=${location.destination}&alternatives=true&key=${process.env.EXPO_PUBLIC_GOOGLE_MAPS_API}`,
-        );
-
-        if (!response.ok) {
-          throw new Error(`HTTP Error ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchRoutes();
-  });
   return (
-    <View>
-      <Link href={"/routes/0" as Href}>
-        <Text>Dashboard Screen</Text>
-      </Link>
-    </View>
+    <ImageBackground
+      source={dashboardgradient}
+      className="flex flex-col items-center h-full w-full py-8"
+    >
+      <View>
+        <Text className="font/[Geist-VariableFont] text-5xl font-semibold text-white py-8">
+          Good Morning!
+        </Text>
+      </View>
+      <Location locationsID={"3"} routeName={"Route"} eta={"15"} />
+      <View className="h-8"></View>
+      <AddLocation locationsID="1"/>
+    </ImageBackground>
   );
 };
 
