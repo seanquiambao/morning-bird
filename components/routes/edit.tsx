9aa083e49c6646d1064cc57b43e0d24ca4c58674
seanquiambao <slquiambao03@gmail.com>
2025-01-25 17:28:17 -0800

    google api connected yay

--- components/routes/edit.tsx
+++ components/routes/edit.tsx
@@ -8,7 +8,9 @@ import {
 } from "react-native";
 import bg1 from "@/assets/images/bg1.png";
 import directions from "@/assets/icons/directions.png";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 const DestinationScreen = () => {
+  console.log(process.env.EXPO_PUBLIC_GOOGLE_MAPS_API);
   return (
     <ImageBackground source={bg1} className="flex flex-col p-4 h-full">
       <View className="flex flex-col gap-4">
@@ -20,12 +22,18 @@ const DestinationScreen = () => {
       </View>
       <View className="flex flex-col gap-4">
         <Text className="font-bold text-white">Location</Text>
-        <TextInput className="bg-white/25 rounded-md p-4" />
-        <Pressable className="border rounded-md border-white">
-          <Text className="text-white font-bold p-5 self-center text-2xl">
-            Submit
-          </Text>
-        </Pressable>
+        <View className="w-full h-[100%] bg-white/50">
+          <GooglePlacesAutocomplete
+            placeholder="Search"
+            onPress={(data, details = null) => {
+              console.log(data, details);
+            }}
+            query={{
+              key: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API,
+              language: "en",
+            }}
+          />
+        </View>
       </View>
     </ImageBackground>
   );
